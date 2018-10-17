7b44fad168146db346e6bea7dbe9ca6509403df1
Sathya Ram <sathyatheram@gmail.com>
2018-10-17 13:09:07 -0400

    added jquery and click event for loading in component and set up routes

--- src/App/App.js
+++ src/App/App.js
@@ -11,8 +11,6 @@ class App extends Component {
       <div>
         <Header />
         <Dashboard />
-        {/* <Cardboard /> */}
-        
       </div>
       );
   }
--- src/PlanningCards/PlanningCards.js
+++ src/PlanningCards/PlanningCards.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route, Link, Switch } from 'react-router-dom'
 import './PlanningCards.css'
 
 class PlanningCards extends Component {
+
   render() {
     return (
       <div>
@@ -10,7 +12,7 @@ class PlanningCards extends Component {
           <p>Click each type for a description</p>
         </div>
         <div className="planningCardGrid">
-          <div className="planningCard">
+          <div id="first" className="planningCard">
             <h3>Conventional</h3>
           </div>
           <div className="planningCard">
@@ -26,40 +28,42 @@ class PlanningCards extends Component {
             <h3>Custom</h3>
           </div>
         </div>
-        <div className="planningCardContent">
-          <div class="planningCardDescription">
-            <h4>Description</h4>
-            <p>
-              This is the most common model of strategic planning, although it is not suited for every organization. It is ideal for organizations that have sufficient resources to pursue very ambitious visions and goals, have external environments that are relatively stable, and do not have a large number of current issues to address. The model usually includes the following overall phases:
-<br/><br/>
-1. Develop or update the mission and optionally, vision and/or values statements.
-<br/><br/>
-2. Take a wide look around the outside and a good look inside the organization, and perhaps update the statements as a result.
-<br/><br/>
-3. As a result of this examination, select the multi-year strategies and/or goals to achieve the vision.
-<br/><br/>
-4. Then develop action plans that specify who is going to do what and by when to achieve each goal.
-<br/><br/>
-5. Identify associated plans, for example, staffing, facilities, marketing and financial plans.
-<br/><br/>
-6. Organize items 1-3 into a Strategic Plan and items 4-6 into a separate one-year Operational Plan.
-            </p>
-          </div>
-          <div class="planningCardModules">
-            <h4>Modules</h4>
-            <ul>
-              <li>Mission</li>
-              <li>Objectives/Goals</li>
-              <li>SWOT Analysis</li>
-              <li>Organizational Strategy</li>
-              <li>Financial Plan</li>
-              <li>Implementation Plan</li>
-            </ul>
-          </div>
-          <div className="buttons">
-              <a href="#">Learn More</a>
-              <a href="#">Choose</a>
+        <div className="planningWrapper">
+          <div className="planningCardContent">
+            <div className="planningCardDescription">
+              <h4>Description</h4>
+              <p>
+                This is the most common model of strategic planning, although it is not suited for every organization. It is ideal for organizations that have sufficient resources to pursue very ambitious visions and goals, have external environments that are relatively stable, and do not have a large number of current issues to address. The model usually includes the following overall phases:
+  <br/><br/>
+  1. Develop or update the mission and optionally, vision and/or values statements.
+  <br/><br/>
+  2. Take a wide look around the outside and a good look inside the organization, and perhaps update the statements as a result.
+  <br/><br/>
+  3. As a result of this examination, select the multi-year strategies and/or goals to achieve the vision.
+  <br/><br/>
+  4. Then develop action plans that specify who is going to do what and by when to achieve each goal.
+  <br/><br/>
+  5. Identify associated plans, for example, staffing, facilities, marketing and financial plans.
+  <br/><br/>
+  6. Organize items 1-3 into a Strategic Plan and items 4-6 into a separate one-year Operational Plan.
+              </p>
             </div>
+            <div className="planningCardModules">
+              <h4>Modules</h4>
+              <ul>
+                <li>Mission</li>
+                <li>Objectives/Goals</li>
+                <li>SWOT Analysis</li>
+                <li>Organizational Strategy</li>
+                <li>Financial Plan</li>
+                <li>Implementation Plan</li>
+              </ul>
+            </div>
+            <div className="buttons">
+                <a href="#">Learn More</a>
+                <Link to="/planningboard/planbuilder">Choose</Link>
+              </div>
+          </div>
         </div>
       </div>
     );
