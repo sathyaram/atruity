c36fea97a57c2f7a73a23eb076cf6ef370580168
Nick Tartaro <ntartaro@gmail.com>
2018-10-18 09:37:55 -0400

    fixed table add button

--- src/PlanBuilder/PlanBuilder.js
+++ src/PlanBuilder/PlanBuilder.js
@@ -2,6 +2,81 @@ import React, { Component } from 'react';
 import './PlanBuilder.css';
 
 class PlanBuilder extends Component {
+
+    newRow() {
+        const addedRow = document.querySelector('tbody')
+        console.log(addedRow);
+        const newRow = `
+            <tr>
+                <td>
+                Goal
+                <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+                <br />
+                Milestone
+                <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+                </td>
+                <td>
+                <input type="text" />
+                </td>
+                <td>
+                <select>
+                    <option>Team</option>
+                    <option>Department</option>
+                    <option>Company</option>
+                </select>
+                </td>
+                <td>
+                Members
+                <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                </select>
+                </td>
+                <td>
+                <select>
+                    <option>Q1</option>
+                    <option>Q2</option>
+                    <option>Q3</option>
+                    <option>Q4</option>
+                </select>
+                to
+                <select>
+                    <option>Q1</option>
+                    <option>Q2</option>
+                    <option>Q3</option>
+                    <option>Q4</option>
+                </select>
+                </td>
+                <td>
+                Status
+                <select>
+                    <option>Complete</option>
+                    <option>In Progress</option>
+                    <option>Incomplete</option>
+                </select>
+                </td>
+            </tr>`;
+        addedRow.insertAdjacentHTML("beforeend", newRow)
+    }
+
   render() {
     return (
       <div className="planBuilder">
@@ -13,7 +88,7 @@ class PlanBuilder extends Component {
           <details className="plan-tool">
             <summary>2. Objectives/Goals</summary>
             <table>
-              <tbody>
+              <tbody className="plan-table">
                 <tr>
                   <th>Label</th>
                   <th>Description</th>
@@ -91,7 +166,7 @@ class PlanBuilder extends Component {
                 </tr>
               </tbody>
             </table>
-            <button id="add">+ ADD</button>
+            <button id="add" onClick={this.newRow}>+ ADD</button>
           </details>
           <details className="plan-tool">
             <summary>3. S.W.O.T Analysis</summary>
